5971e578366dc8c65048fe8222af1dde9b01b633
Michael Argentini <michael@argentini.us>
2025-01-19 19:11:54 -0500

    Added notch1/2 to Rotor

--- Enigma/Rotor.cs
+++ Enigma/Rotor.cs
@@ -9,12 +9,16 @@ public sealed class Rotor
 {
     public int RingPosition { get; private set; }
     public int Rotation { get; private set; }
+    public char Notch1 { get; set; } = '\0';
+    public char Notch2 { get; set; } = '\0';
     public Dictionary<char,char> Wheel => _wheel;
 
     private Dictionary<char,char> _wheel { get; set; } = [];
     private bool IsInitialized { get; set; }
     private IndexedDictionary<char,char> EncipherWheel { get; set; } = new();
 
+    #region Configuration
+    
 	private void Initialize()
 	{
         EncipherWheel.Clear();
@@ -24,13 +28,29 @@ public sealed class Rotor
 
         EncipherWheel.AddRange(_wheel, RingPosition);
 
+        if (Notch1 == '\0')
+            Notch1 = _wheel.First().Value;
+        
         IsInitialized = true;
     }
 
-	public Rotor SetWheel(Dictionary<char,char> value)
+	public Rotor SetWheel(Dictionary<char,char> value, char notch1 = '\0', char notch2 = '\0')
 	{
+        if (value.Count == 0)
+            throw new Exception("Rotor.SetWheel() => Value is empty");
+
         _wheel = value;
 
+        if (_wheel.ContainsKey(notch1))
+            Notch1 = notch1;
+        else
+            Notch1 = _wheel.First().Value;
+
+        if (_wheel.ContainsKey(notch2) && notch2 != Notch1)
+            Notch2 = notch2;
+        else
+            Notch2 = '\0';
+        
         Initialize();
 
         return this;
@@ -45,17 +65,7 @@ public sealed class Rotor
 
         return this;
 	}
-
-	public Rotor Rotate()
-	{
-        Rotation++;
-
-        if (Rotation >= _wheel.Count)
-            Rotation = 0;
-
-        return this;
-	}
-
+    
 	public Rotor SetRingPosition(int value)
 	{
         if (value >= 0 && value < _wheel.Count)
@@ -67,6 +77,20 @@ public sealed class Rotor
 
         return this;
 	}
+
+    #endregion
+    
+    #region Actions
+    
+    public Rotor Rotate()
+    {
+        Rotation++;
+
+        if (Rotation >= _wheel.Count)
+            Rotation = 0;
+
+        return this;
+    }
 
 	public char SendCharacter(char c)
 	{
@@ -93,4 +117,6 @@ public sealed class Rotor
 
         return EncipherWheel.KeyValues[rotatedIndex].Key;
 	}
+    
+    #endregion
 }
